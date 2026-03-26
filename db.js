a6a1fb98d01f5e856588e39b230623b160d5a32a
Ediz <nof4ce1337@gmail.com>
2026-03-26 17:00:01 +0300

    Auto sync Per 26.03.2026 17:00:01,25

---
+++ db.js
@@ -0,0 +1,37 @@
+// --------------------------------------------------------
+// ediz - mysql baglanti
+// --------------------------------------------------------
+
+const mysql = require("mysql2/promise");
+const yapilandirma = require("../yapilandirma");
+
+var havuz = null;
+
+async function veritabaniBaglan() {
+    try {
+        havuz = mysql.createPool({
+            host: yapilandirma.mysql.sunucu,
+            port: yapilandirma.mysql.port,
+            user: yapilandirma.mysql.kullanici,
+            password: yapilandirma.mysql.sifre,
+            database: yapilandirma.mysql.veritabani,
+            waitForConnections: true,
+            connectionLimit: 20,
+            charset: "utf8mb4"
+        });
+
+        var b = await havuz.getConnection();
+        b.release();
+        console.log("[ediz] mysql baglantisi basarili");
+    } catch (h) {
+        console.error("[ediz] mysql hatasi:", h.message);
+        process.exit(1);
+    }
+}
+
+function havuzGetir() {
+    if (!havuz) throw new Error("[ediz] havuz hazir degil");
+    return havuz;
+}
+
+module.exports = { veritabaniBaglan, havuzGetir };
